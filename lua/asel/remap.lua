56f3bdf050e88031a033efe6d9c3a12c54a89c2e
AselGamer <aselfernandez@gmail.com>
2024-11-07 22:20:27 +0100

    added some remaps i use at work

--- lua/asel/remap.lua
+++ lua/asel/remap.lua
@@ -2,3 +2,4 @@ vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 vim.keymap.set('t', '<C-w>h', '<C-\\><C-n><C-w>h',{silent = true})
 vim.keymap.set({'n', 'x'}, '<leader>ff', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+vim.keymap.set('n', '<leader>s', [[:%s/<C-r><C-w>//gc<Left><Left><Left>]], { noremap = true, silent = false })
